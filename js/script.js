2a1d65ca5fabb01bb4e6ab8517e2dd3dda7a8872
Michal Schiller <mschiller890@gmail.com>
2026-07-16 09:56:36 +0200

    UI update, apps are almost real
    starting to look like an os now!

--- js/script.js
+++ js/script.js
@@ -6,5 +6,22 @@ function updateTime() {
     document.getElementById('time').textContent = time;
 }
 
+const wallpapers = {
+    // from https://wall.alphacoders.com/big.php?i=1315811
+    miku: 'assets/wallpapers/miku.jpg',
+    meiko: 'assets/wallpapers/meiko.jpg',
+    kaito: 'assets/wallpapers/kaito.jpg',
+    kagaminetwins: 'assets/wallpapers/kagaminetwins.jpg',
+    gumi: 'assets/wallpapers/gumi.jpg'
+};
+
+function setBackgroundImage(name = 'miku') {
+    document.body.style.backgroundImage = `url(${wallpapers[name]})`;
+    document.body.style.backgroundSize = 'cover';
+    document.body.style.backgroundPosition = 'center';
+    document.body.style.backgroundRepeat = 'no-repeat';
+}
+
 updateTime();
 setInterval(updateTime, 1000);
+setBackgroundImage();
